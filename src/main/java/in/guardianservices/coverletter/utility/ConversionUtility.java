4a30dcda74c25d891e00b1abd2b7593dbcb86baa
rahulsharma <rsharma0374@gmail.com>
2025-04-16 22:47:06 +0530

    updated image conversion code

--- src/main/java/in/guardianservices/coverletter/utility/ConversionUtility.java
+++ src/main/java/in/guardianservices/coverletter/utility/ConversionUtility.java
@@ -3,19 +3,30 @@ package in.guardianservices.coverletter.utility;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.core.io.ClassPathResource;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.util.Base64;
 
 public class ConversionUtility {
 
-    public static String convertImageToBase64Code(String imagePath) throws Exception{
+//    public static String convertImageToBase64Code(String imagePath) throws Exception{
+//
+//        String base64Code = null;
+//        if (StringUtils.isNotBlank(imagePath)) {
+//            byte[] tickMarkByte = Files.readAllBytes(new ClassPathResource(imagePath).getFile().toPath());
+//            base64Code = new String(Base64.getEncoder().encode(tickMarkByte), StandardCharsets.UTF_8);
+//        }
+//        return base64Code;
+//    }
 
-        String base64Code = null;
-        if (StringUtils.isNotBlank(imagePath)) {
-            byte[] tickMarkByte = Files.readAllBytes(new ClassPathResource(imagePath).getFile().toPath());
-            base64Code = new String(Base64.getEncoder().encode(tickMarkByte), StandardCharsets.UTF_8);
+    public static String convertImageToBase64Code(String imagePath) throws IOException {
+        ClassPathResource resource = new ClassPathResource(imagePath);
+        try (InputStream inputStream = resource.getInputStream()) {
+            byte[] bytes = inputStream.readAllBytes();
+            return Base64.getEncoder().encodeToString(bytes);
         }
-        return base64Code;
     }
+
 }
